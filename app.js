e48549120667ff542421037d2cd134da83b58c7a
dannyflatiron <dannyflatiron@gmail.com>
2021-02-01 22:58:53 -0500

    feat: create game board & set card to flip

--- app.js
+++ app.js
@@ -61,11 +61,35 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
   const grid = document.querySelector('.grid')
+  let cardsChosen =  []
+  let cardsChosenId = []
 
   // create game board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       let card = document.createElement('img')
+      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('data-id', i + 1)
+      // card.addEventListener('click', flipCard)
+      grid.appendChild(card)
     }
   }
+
+  // flip card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
+
+  // check for match
+  function checkForMatch() {
+
+  }
+
+  createBoard()
 })
